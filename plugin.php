75603039fcca0d50a9058b69893c83cca86d6eb7
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:56:28 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_simplifying_fractions_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Simplifying Fractions Calculator by Calculator.iO";
 
 function display_ci_simplifying_fractions_calculator(){
